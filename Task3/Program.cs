c6020c09bb6c8b3ee8d1f8de574eca949f5108a3
Sergey Rombak <rombak.a87@mail.ru>
2023-11-29 18:59:01 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача3 
+/*Задайте массив из вещественных чисел с ненулевой дробной частью. 
+Найдите разницу между максимальным и минимальным элементов массива.*/
+
+static double[] AFAWNZD(int length) 
+{
+    double[] array = new double[length];
+    Random random = new Random();
+
+    for (int i = 0; i < length; i++)
+    {
+        double randomNumber = Math.Round(random.NextDouble() * 10, 2);
+        array[i] = randomNumber;
+    }
+    return array;
+}
+
+void PrintArray(double[] InMass)
+{
+    Console.Write("Массив с элементами: ");
+    for (int i = 0; i < InMass.Length; i++)
+    {
+        Console.Write(InMass[i] + " ");
+    }
+}
+
+void DON(double[] InMass) // DON = DifferenceOofNnumbers
+{
+    double max = InMass[0];
+    double min = InMass[0];
+
+    foreach (double num in InMass)
+    {
+        if (num > max)
+            max = num;
+
+        if (num < min)
+            min = num;
+    }
+
+    double difference = max - min;
+
+    Console.WriteLine();
+    Console.WriteLine($"Максимальный элемент: {max}");
+    Console.WriteLine($"Минимальный элемент: {min}");
+    Console.WriteLine($"Разница: {difference}");
+}
+
+int arrayLength = 25;
+double[] myArray = AFAWNZD(arrayLength);
+PrintArray(myArray);
+DON(myArray);
